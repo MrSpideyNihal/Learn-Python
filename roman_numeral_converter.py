f380ff361633f81f244331ba0ce39950d4e0125b
MrspdieyNihal <Nihalrodge01@gmail.com>
2022-01-05 16:26:00 +0000

    Convert to roman numerals

---
+++ roman_numeral_converter.py
@@ -0,0 +1,25 @@
+"""Convert integer to Roman numeral."""
+import string
+
+def int_to_roman(num):
+    """Return the Roman numeral representation of the input number."""
+    val = [1000, 900, 500, 400,
+           100, 90, 50, 40,
+           10, 9, 5, 4,
+           1]
+    syb = ["M", "CM", "D", "CD",
+          "C", "XC", "L", "XL",
+          "X", "IX", "V", "IV",
+          "I"]
+    roman_num = ''
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syb[i]
+            num -= val[i]
+        i += 1
+    return roman_num
+
+if __name__ == "__main__":
+    num = int(input("Enter an integer: "))
+    print(f"Roman numeral representation of {num} is {int_to_roman(num)}")
