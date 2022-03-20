abbe29e4107b3402323dcebf413282a801a4de75
MrspdieyNihal <Nihalrodge01@gmail.com>
2022-03-20 19:11:00 +0000

    Palindrome checker

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,10 +1,16 @@
-"""Check if a given string is the same backwards as forwards."""
+"""Check for palindromes."""
 import re
 
 def is_palindrome(s):
-    """Return True if s is a palindrome, False otherwise."""
-    s = re.sub(r'\s+', '', s).lower()
-    return s == s[::-1]
+    """Return True if the input string is a palindrome, False otherwise."""
+    return s.lower() == s.lower()[::-1]
 
 if __name__ == "__main__":
-    print(is_palindrome(input("Enter a string: ")))
+    while True:
+        user_input = input("Enter a word or 'q' to quit: ")
+        if user_input.lower() == 'q':
+            break
+        elif is_palindrome(user_input):
+            print(f"'{user_input}' is a palindrome!"
+        else:
+            print(f"'{user_input}' is not a palindrome."
