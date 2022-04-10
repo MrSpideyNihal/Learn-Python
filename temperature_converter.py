83c91db1e5cd9709b660fe7ce939722eefa6d4e7
MrspdieyNihal <Nihalrodge01@gmail.com>
2022-04-10 20:29:00 +0000

    Convert temperature units

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,9 +1,15 @@
 """Temperature converter."""
+import math
 
 def convert_temperature(celsius):
-    """Return Fahrenheit equivalent of Celsius temperature."""
-    return (celsius * 9/5) + 32
+    """Convert Celsius to Fahrenheit or Kelvin."""
+    return celsius * 9/5 + 32, celsius + 273.15
 
 if __name__ == "__main__":
-    celsius = float(input("Enter temperature in Celsius: "))
-    print(f"{celsius}°C is {convert_temperature(celsius)}°F")
+    while True:
+        try:
+            celsius = float(input("Enter temperature in Celsius: "))
+            fahrenheit, kelvin = convert_temperature(celsius)
+            print(f"Fahrenheit: {fahrenheit:.2f}", f"Kelvin: {kelvin:.2f}"`
+        except ValueError:
+            print("Invalid input. Try again!")
