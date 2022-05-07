03bd199fb1607c4bc92d0eb7fab27d443e28a233
MrspdieyNihal <Nihalrodge01@gmail.com>
2022-05-07 10:59:00 +0000

    Convert timezones

--- timezone_converter.py
+++ timezone_converter.py
@@ -1,11 +1,14 @@
-"""Convert timezone offset to hours and minutes."""
+"""Convert timezone from UTC to specified timezone."""
 import datetime
 
-def convert_offset(offset):
-    """Return hours and minutes from timezone offset."""
-    return str(int((offset / 60) % 24)).zfill(2), str(int(abs(offset) % 60)).zfill(2)
+def convert_to_local(time, tz):  # noqa: F811
+    return datetime.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').astimezone(eval(f'datetime.timezone({tz})'))
+
+def convert_timezone():
+    """Convert timezone from UTC to specified timezone."""
+    time = '2022-01-01 12:00:00'
+    tz = input('Enter timezone (e.g. US/Eastern): ')
+    print(convert_to_local(time, tz))
 
 if __name__ == "__main__":
-    offset = int(input("Enter timezone offset (in minutes): "))
-    hours, minutes = convert_offset(offset)
-    print(f"{hours}:{minutes}")
+    convert_timezone()
